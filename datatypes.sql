d4df6e0460a4e2fb0183d838fc580ab800487eb8
Shweta Joshi <93914795+shwetaj2820@users.noreply.github.com>
2022-01-12 18:29:48 +0530

    using DATE , TIME

--- datatypes.sql
+++ datatypes.sql
@@ -1,11 +1,17 @@
-# FLOAT and DOUBLE ARE floating types and calculations are approximate - hence store larger numbers with less space
-# DECIMAL is fixed point and calculation is exact
+# DATE -values with a date but without time 'YYYY-MM-DD'
+# TIME -values with a time 'HH-MM-SS'
+# DATETIME -values with date and time 'YYYY-MM-DD HH-MM-SS'
 
-# float - 4 bytes - precision issue after 7 digits
-# double - 8 bytes - precision issue after 15 digits
+CREATE TABLE people (name VARCHAR(100), birthdate DATE, birthtime TIME, birthdt DATETIME);
+ 
+INSERT INTO people (name, birthdate, birthtime, birthdt)
+VALUES('Padma', '1983-11-11', '10:07:35', '1983-11-11 10:07:35');
+ 
+INSERT INTO people (name, birthdate, birthtime, birthdt)
+VALUES('Larry', '1943-12-25', '04:10:42', '1943-12-25 04:10:42');
 
-CREATE TABLE things(price FLOAT);
+# CURDATE()-gives current date,CURTIME()-gives current time,NOW()-gives current datetime
+INSERT INTO people (name, birthdate, birthtime, birthdt)
+VALUES('Toaster',CURDATE(),CURTIME(),NOW());
 
-INSERT INTO things(price) VALUES(88.45),(8877.45),(8877665544.45);
-
-SELECT * FROM things;
+SELECT * FROM people;
