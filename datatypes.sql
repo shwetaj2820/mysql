7a995c20db82db837619134fa89afb2811158c3e
Shweta Joshi <93914795+shwetaj2820@users.noreply.github.com>
2022-01-13 11:57:13 +0530

    DATE_FORMAT

--- datatypes.sql
+++ datatypes.sql
@@ -1,17 +1,11 @@
-# DATE -values with a date but without time 'YYYY-MM-DD'
-# TIME -values with a time 'HH-MM-SS'
-# DATETIME -values with date and time 'YYYY-MM-DD HH-MM-SS'
+# date formatting
 
-CREATE TABLE people (name VARCHAR(100), birthdate DATE, birthtime TIME, birthdt DATETIME);
- 
-INSERT INTO people (name, birthdate, birthtime, birthdt)
-VALUES('Padma', '1983-11-11', '10:07:35', '1983-11-11 10:07:35');
- 
-INSERT INTO people (name, birthdate, birthtime, birthdt)
-VALUES('Larry', '1943-12-25', '04:10:42', '1943-12-25 04:10:42');
+SELECT DATE_FORMAT('2000-09-28 22:23:00' , '%W%M%Y');    #using specifiers
 
-# CURDATE()-gives current date,CURTIME()-gives current time,NOW()-gives current datetime
-INSERT INTO people (name, birthdate, birthtime, birthdt)
-VALUES('Toaster',CURDATE(),CURTIME(),NOW());
+SELECT CONCAT(MONTHNAME(birthdate),' ',DAY(birthdate),' ',YEAR(birthdate)) FROM people;
 
-SELECT * FROM people;
+SELECT name,DATE_FORMAT(birthdt,'was born on %W') FROM people;
+
+SELECT DATE_FORMAT(birthdt,'%d/%m/%Y') FROM people;
+
+SELECT DATE_FORMAT(birthdt,'%d/%m%/%Y at %h:%i') FROM people;
