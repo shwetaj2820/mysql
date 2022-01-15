d62db7f7b5782fca4997de8973bfdbdb0979f887
Shweta Joshi <93914795+shwetaj2820@users.noreply.github.com>
2022-01-15 19:38:34 +0530

    IN, NOT IN

--- datatypes.sql
+++ datatypes.sql
@@ -1,13 +1,15 @@
-# BETWEEN X AND Y
-# select all books published between 2004 and 2015
+# using IN 
+# select all books written by Carver or Lahiri or Smith
 
-SELECT title,released_year FROM books WHERE released_year>=2004 AND released_year<=2015 ORDER BY released_year;
+SELECT title,author_lname FROM books WHERE author_lname='Carver' OR author_lname='Lahiri' OR author_lname='Smith';
 
-SELECT title,released_year FROM books WHERE released_year BETWEEN 2004 AND 2015 ORDER BY released_year;
+SELECT title,author_lname FROM books WHERE author_lname IN('Carver','Lahiri','Smith');
 
-# NOT BETWEEN X AND Y
-# select all books not published between 2004 and 2015
+# using NOT IN 
+# select all books where release year is not even 
+SELECT title,released_year FROM books WHERE released_year NOT IN (2000,2002,2004,2006,2008,2010,2012,2014,2016);
 
-SELECT title,released_year FROM books WHERE released_year<=2004 OR released_year>=2015 ORDER BY released_year;
+SELECT title,released_year FROM books WHERE released_year>=2000 AND released_year NOT IN (2000,2002,2004,2006,2008,2010,2012,2014,2016) 
+ORDER BY released_year;
 
-SELECT title,released_year FROM books WHERE released_year NOT BETWEEN 2004 AND 2015 ORDER BY released_year;
+SELECT title,released_year FROM books WHERE released_year>=2000 AND released_year%2 != 0 ORDER BY released_year;
