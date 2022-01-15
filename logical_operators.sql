9ccc3d4ff9bc05ddc0768873650c6f55176bfa0d
Shweta Joshi <93914795+shwetaj2820@users.noreply.github.com>
2022-01-15 18:55:32 +0530

    AND,OR

--- logical_operators.sql
+++ logical_operators.sql
@@ -1,35 +1,13 @@
-#     EQUALITY OPERATOR: =
-SELECT * FROM books WHERE released_year=2017;
+# logical AND - all conditions are true also can use &&
+SELECT title FROM books WHERE author_lname='Eggers';
+SELECT title FROM books WHERE released_year>2010;
+SELECT title,author_lname,released_year FROM books WHERE author_lname='Eggers' && released_year>2010;
 
-SELECT title,author_lname FROM books WHERE author_lname = 'Harris';
+SELECT title,author_lname,released_year FROM books WHERE author_lname = 'Eggers' AND released_year>2010 AND title LIKE '%novel%';
 
-#     INEQUALITY OPERATOR: !=
-SELECT * FROM books WHERE released_year!=2017;
+# logical OR  - at least one condition is true also can use ||
+SELECT title FROM books WHERE author_lname='Eggers';
+SELECT title FROM books WHERE released_year>2010;
+SELECT title,author_lname,released_year FROM books WHERE author_lname='Eggers' OR released_year>2010;
 
-SELECT title,author_lname FROM books WHERE author_lname != 'Harris';
-
-#     NOT LIKE
-SELECT title FROM books WHERE title LIKE 'W%'; # MATCHES THE PATTERN
-
-SELECT title FROM books WHERE title NOT LIKE '%W%'; 
-
-#     COMPARISON OPERATORS 
-SELECT title , released_year FROM books WHERE released_year<2017 ORDER BY released_year;
-
-SELECT title , stock_quantity FROM books WHERE stock_quantity >=100;
-
-SELECT title FROM books WHERE released_year<2000 ORDER BY released_year;
-
-# 	boolean logic - output 0/1
-SELECT 99>1; #output: 1 -TRUE
-SELECT 100<84; #output: 0-FALSE
-SELECT 'a'>'b';
-SELECT 'A'>'a';
-SELECT 3<-10;
-SELECT -10<-9;
-SELECT 42<=42;
-SELECT 'h'<'p';
-SELECT 'Q'<='q';
-SELECT author_lname FROM books WHERE author_lname = 'Eggers';
-SELECT author_lname FROM books WHERE author_lname = 'eGGers';
-
+SELECT title,author_lname,released_year FROM books WHERE author_lname = 'Eggers' OR released_year>2010 OR title LIKE '%novel%';
