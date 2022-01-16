853cea72df64c7a2eb7f6cffbc7cc72382d61d3a
Shweta Joshi <93914795+shwetaj2820@users.noreply.github.com>
2022-01-16 23:44:52 +0530

    innerjoin example, left join

--- joins.sql
+++ joins.sql
@@ -1,30 +1,35 @@
-# only select rows where userid = customerid
+SELECT first_name, last_name, order_date, amount 
+FROM customers
+JOIN orders
+	ON customers.id = orders.customer_id
+ORDER BY order_date;
 
-# SELECT * FROM customers,orders WHERE id = customer_id; - error: column id is ambiguous
+SELECT first_name, last_name, order_date, SUM(amount) AS amount_spent 
+FROM customers
+JOIN orders
+	ON customers.id = orders.customer_id
+GROUP BY orders.customer_id
+ORDER BY amount_spent DESC;
 
-# hence pre-pend the table name before the column name //only take overlapping conditions
-# join data based on a condition
-# IMPLICIT INNER JOIN
-SELECT * FROM customers,orders 
-	WHERE customers.id = orders.customer_id;
-
-SELECT first_name,last_name,order_date,amount FROM customers,orders 
-	WHERE customers.id = orders.customer_id;
-
-# EXPLICIT INNER JOIN
-SELECT * FROM customers 
-JOIN orders 
-	ON customers.id = orders.customer_id;
-
-SELECT first_name,last_name,order_date,amount FROM customers 
-JOIN orders 
-	ON customers.id = orders.customer_id;
-	
-SELECT first_name,last_name,order_date,amount FROM orders
-JOIN customers
-	ON orders.customer_id = customers.id;
-	
-# arbitrary example
+# INNER JOIN (WAY 2) 
 SELECT * FROM customers
-JOIN orders 
+INNER JOIN orders 
 	ON customers.id = orders.customer_id;
+	
+# LEFT JOIN 
+SELECT * FROM customers
+LEFT JOIN orders
+	ON customers.id = orders.customer_id;
+
+SELECT first_name,last_name,order_date,amount FROM customers
+LEFT JOIN orders
+	ON customers.id = orders.customer_id;
+	
+SELECT
+	first_name,
+	last_name,
+	IFNULL(amount,0) AS amount_spent
+FROM customers
+LEFT JOIN orders 
+	ON customers.id = orders.customer_id
+GROUP BY customer_id ORDER BY amount_spent;
